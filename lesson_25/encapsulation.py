39b017bce87dfa5457c7ad67d5a2b9692970c306
Willoww <0654313@schoolsnet.act.edu.au>
2026-04-28 16:00:32 +1000

    added lesson 25

---
+++ lesson_25/encapsulation.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    # initializer method
+    # called automatically
+    def __init__(self, name, value, acctype):
+        self.__value = value # Needs to be private
+        self.name = name # Okay to keep public
+        self.__acctype = acctype
+
+    def display(self):
+        print(f"Account name:   {self.name}")
+        print(f"Balance:        ${self.__value}")
+        print(f"Account type:   {self.__acctype}")
+
+    def withdraw(self, change):
+        if change < self.__value:
+            self.__value -= change
+        else:
+            raise Exeption
+
+################################################
+# pretend this is a seperate file
+
+acct = BankAccount("Jadey", 10000000, "Checking")
+acct.display()
+acct.withdraw(100)
+acct.display()
